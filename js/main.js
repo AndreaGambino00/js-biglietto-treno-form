42ed59a6fdd762701b1333b073ba9ea2eb892e9a
Andrea <andreagambinomarketing@gmail.com>
2021-07-22 16:52:49 +0200

    Parte logica finita

--- js/main.js
+++ js/main.js
@@ -5,16 +5,53 @@ btnGenera.addEventListener('click',
     function() {
         var nome = document.getElementById('nome').value;
         var km = document.getElementById('km').value;
-        var fasciaEta = document.getElementById('fascia-eta').value;
+        var eta = document.getElementById('eta').value;
+        console.log(nome, km, eta);
 
         var biglietto = document.getElementById('biglietto');
         biglietto.style.display = "block";
+        //Nome utente
+        document.getElementById('nome-utente').innerHTML = nome;
+        //Offerta utente
+        document.getElementById('offerta-utente').innerHTML = eta;
+        //Carrozza
+        var NumCarrozza= Math.floor(Math.random() * 20 + 1);
+        document.getElementById('num-carrozza').innerHTML = NumCarrozza;
+        //Codice CP
+        var CodiceUtente= Math.floor(Math.random() * (100000 -90000) + 90000);
+        document.getElementById('cod-cp').innerHTML = CodiceUtente;
+        //Costo biglietto
+        var eta = document.getElementById('eta').value;
+        var prezzoalKm = 0.21;
 
-        document.getElementById('nome').innerHTML = 'Nome:';
+        if (eta < 18) 
+        {
+            var under18 = prezzoalKm * km * 0.8;
+            document.getElementById('prezzo-biglietto').innerHTML = ' ' + under18.toFixed(2) + '€';
+        }
+        
+        else if (eta >= 65) 
+        {
+            var over65 = prezzoalKm * km * 0.6;
+            document.getElementById('prezzo-biglietto').innerHTML = ' ' + over65.toFixed(2) + '€';
+        }
+        
+        else 
+        {
+            var prezzostandard = prezzoalKm * km;
+            document.getElementById('prezzo-biglietto').innerHTML = ' ' + prezzostandard.toFixed(2) + '€';
+        }
+
+
+
     }
+
+    
 
 )
 
+
+//Annullo inserimenti
 var btnAnnulla = document.getElementById('annulla')
 
 btnAnnulla.addEventListener('click',
@@ -24,7 +61,9 @@ btnAnnulla.addEventListener('click',
 
         document.getElementById('nome').value = ''
         document.getElementById('km').value = ''
-        document.getElementById('fascia-eta').value = ''
+        document.getElementById('eta').value = ''
     }
 )
+
+
 
